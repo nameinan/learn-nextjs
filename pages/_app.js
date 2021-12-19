146f654921592054df790b80bd357697e353c7d2
Nandakumar Nameirakpam <nandakumar.nameirakpam@tietoevry.com>
2021-12-19 07:17:48 +0530

    create a nextjs project , add footer and hello page

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,13 @@
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <div>   
+      <Component {...pageProps} />
+      <footer> 
+          <p>@Nandakumar</p>
+      </footer>
+    </div>;
+  
 }
 
 export default MyApp
---
+++ readme-description.txt
@@ -0,0 +1,5 @@
+1. create hello.js
+2. add custom footer
+   * add to _app for the common
+
+3. Fash refresh
